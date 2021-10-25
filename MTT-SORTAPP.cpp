917adcd7d5977a615da328df1fec5d89ac0ec806
Eric <eric.arreola@sjsu.edu>
2021-10-24 18:55:31 -0700

    Outline update

--- MTT-SORTAPP.cpp
+++ MTT-SORTAPP.cpp
@@ -1,2 +1,37 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <vector>
+#include <stdlib.h>
+#include <unistd.h>
+ //<WIP> Everything subject to change including functions
+
+//Function: Merge,MergeSort,Semaphore or Mutex, Main, Thread Creation?
+// Start out with 4 threads and just reuse like in a thread pool?
+
+//Things to consider: thread pool, global array, synchronization
+
+//merge the different subarrays handled by threads
+void merge() {
+
+}
+
+//actual mergesort algorithm
+void mergesort() {
+
+}
+
+//prevents race conditions using mutex or semaphore locks
+void lock() { //can rename
+
+}
+
+//function that creates threads/manages threads
+void threadcreation() { //rename later
+
+}
+
+//generates input,displays output,maybe measure performance in some way
+int main() {
+
+    return 0;
+}
